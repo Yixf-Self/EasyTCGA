3880c0f1dd22ed4aedc1a342b52405585ce49a45
Viktoria Kliewer <kliewer@ls8olc09>
2016-09-22 13:41:43 +0200

    tech report

--- TechnicalReport/glmnet_example/ex.R
+++ TechnicalReport/glmnet_example/ex.R
@@ -1,5 +1,4 @@
 library(glmnet)
-
 n = 100 
 p = 200
 
@@ -9,7 +8,6 @@ y[(n/2):n] = "red"
 y = factor(y) # label to "factor"
 y_names = seq(n,1,-1)
 
-# data = dn_miRSeq(miRNA_ID[sample.int(2588,100)], "ACC", "", page.Size=500, sort_by="tcga_participant_barcode")
 x = matrix(rnorm(n*p), nrow=n, ncol=p)
 x_names = seq(1,n,1)
 
@@ -26,3 +24,4 @@ c = coef(obj)
 idx = which(abs(c[-1, 10])>0)
 
 # print the names of miRNA corresponding to the indices
+
